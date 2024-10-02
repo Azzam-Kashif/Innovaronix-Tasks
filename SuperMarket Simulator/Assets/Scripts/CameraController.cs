32ffc18a3f1dee6793c5cdc4ae9ece3354cc0803
Azzam-Kashif <azzamkashif999@gmail.com>
2024-10-02 16:38:54 +0500

    Third Person Controller.

--- SuperMarket Simulator/Assets/Scripts/CameraController.cs
+++ SuperMarket Simulator/Assets/Scripts/CameraController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform followTarget;
+
+    [SerializeField] float rotationSpeed = 2f;
+
+    [SerializeField] float distance = 5;
+
+    [SerializeField] float minVerticalAngle = -45;
+
+    [SerializeField] float maxVerticalAngle = 45;
+
+    [SerializeField] Vector2 framingoffset;
+
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY;
+
+
+
+
+    float rotationX;
+    float rotationY;
+
+    float invertXVal;
+    float invertYVal;
+
+
+    private void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        invertXVal = (invertX) ? - 1 : 1;
+        invertYVal = (invertY) ? - 1 : 1;
+
+        rotationX += Input.GetAxis("Mouse Y") * invertYVal * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+
+        rotationY += Input.GetAxis("Mouse X") * invertXVal * rotationSpeed;
+
+
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        var focusPosition = followTarget.position + new Vector3(framingoffset.x, framingoffset.y);
+
+        transform.position = focusPosition - targetRotation * new Vector3(0, 0, distance);
+        transform.rotation = targetRotation;
     }
+
+    public Quaternion PlanarRotation => Quaternion.Euler(0, rotationY, 0);
 }
