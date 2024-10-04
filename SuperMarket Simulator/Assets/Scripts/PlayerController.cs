1b03b966dd454d2e0cb21aeb97b37802e5b57acd
Azzam-Kashif <azzamkashif999@gmail.com>
2024-10-04 10:58:54 +0500

    Joystick for Character Movement

--- SuperMarket Simulator/Assets/Scripts/PlayerController.cs
+++ SuperMarket Simulator/Assets/Scripts/PlayerController.cs
@@ -1,50 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float walkSpeed = 3f;
+    [SerializeField] float runSpeed = 6f;
     [SerializeField] float rotationSpeed = 500f;
+    [SerializeField] Joystick joystick;
 
-    [SerializeField] float groundCheckRadius = 0.2f;
-    [SerializeField] Vector3 groundCheckOffset;
-    [SerializeField] LayerMask groundLayer;
-
-    bool isGrounded;
+    [SerializeField] float walkSpeedThreshold = 0.5f;
 
     Quaternion targetRotation;
 
     CameraController cameraController;
     Animator animator;
-    CharacterController characterController;
+    Rigidbody rb;
+
+    float currentMoveAmount = 0f;
 
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
         animator = GetComponent<Animator>();
-        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        rb.freezeRotation = true;
+
+        animator.applyRootMotion = false;
+    }
+
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
 
-        float moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
+        float h = joystick.Horizontal;
+        float v = joystick.Vertical;
 
-        var moveInput = (new Vector3(h, 0, v)).normalized;
+        Vector2 input = new Vector2(Mathf.Clamp(h, -1f, 1f), Mathf.Clamp(v, -1f, 1f));
+        float inputMagnitude = Mathf.Clamp01(input.magnitude);
 
-        var moveDir = cameraController.PlanarRotation * moveInput;
-        
-        if(moveAmount > 0)
+        if (inputMagnitude > 0.1f)
         {
-           characterController.Move(moveDir * moveSpeed * Time.deltaTime);
-           targetRotation = Quaternion.LookRotation(moveDir);
+            float moveSpeed = inputMagnitude < walkSpeedThreshold ? walkSpeed : runSpeed;
+
+            DOTween.To(() => currentMoveAmount, x => currentMoveAmount = x, inputMagnitude, 0.1f).SetEase(Ease.OutQuad);
+            animator.SetFloat("moveAmount", currentMoveAmount);
+
+            Vector3 moveInput = new Vector3(h, 0, v).normalized;
+            Vector3 moveDir = cameraController.PlanarRotation * moveInput;
+
+            rb.velocity = moveDir * moveSpeed;
+
+            targetRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
-
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-        animator.SetFloat("moveAmount", moveAmount, 0.1f, Time.deltaTime);
+        else
+        {
+            rb.velocity = Vector3.zero;
+            animator.SetFloat("moveAmount", 0f);
+        }
     }
-
 }
